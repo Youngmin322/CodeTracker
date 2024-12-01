bd6ea9f221e8f2b04d7b18f811c85c64a6a8d09e
Youngmin322 <choyoungmin0322@gmail.com>
2024-12-01 14:08:27 +0900

    Initial Commit

---
+++ CodeTracker/CodeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTrackerApp.swift
+//  CodeTracker
+//
+//  Created by 조영민 on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
